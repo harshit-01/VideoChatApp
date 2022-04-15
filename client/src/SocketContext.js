db8e8e50766c3d0a828099e0c0d124ea39616db7
harshit-01 <kharshit0101@gmail.com>
2022-04-16 01:05:54 +0530

    deployed link added

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -3,7 +3,7 @@ import Peer from "simple-peer";
 import {io} from "socket.io-client";
 
 const SocketContext = createContext();
-const socket = io('http://localhost:5000');
+const socket = io('https://tieup-video-feature.herokuapp.com/');
 
 const ContextProvider = ({children})=>{
    
